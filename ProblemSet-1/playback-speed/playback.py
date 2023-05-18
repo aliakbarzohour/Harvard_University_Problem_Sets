b700bba4021fff91f60ac72776a80416394ad162
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-05-18 23:44:42 +0330

    play back description

--- ProblemSet-1/playback-speed/playback.py
+++ ProblemSet-1/playback-speed/playback.py
@@ -1,3 +1,12 @@
+# ===============================================
+
+# What should I have done?
+
+# Write a program that receives input from the user and then output
+# The program should be the same input with three dots as a space (...).
+
+# ===============================================
+
 input = input("[@] Just Type Anything : ").replace(' ', '...')
 
 print(input)
