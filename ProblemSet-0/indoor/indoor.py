bf09a676bfc89fb9280b845eff8a149876eed527
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-05-18 23:34:57 +0330

    indoor description

--- ProblemSet-0/indoor/indoor.py
+++ ProblemSet-0/indoor/indoor.py
@@ -1,3 +1,17 @@
+# ===============================================
+# ==== WRITING IN ALL CAPS IS LIKE YELLING ======
+# ===============================================
+
+# What should I have done?
+
+# Write a program that receives an input from the user and then
+# returns the same input with Return lowercase letters as output.
+# Punctuation and spacing between words should appear unchanged in the output.
+# Also, you don't need to explicitly receive input from the user,
+# and you can add a string to Give the title of the input argument
+# to the program when running the code.
+
+# ===============================================
 
 input = input("[^] Just Type Anything : ")
 
