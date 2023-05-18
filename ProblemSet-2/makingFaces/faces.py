c45c4a3ed5a20f24b5ec97380ceec397c71c8e30
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-05-18 23:56:28 +0330

    making faces description and output preview

--- ProblemSet-2/makingFaces/faces.py
+++ ProblemSet-2/makingFaces/faces.py
@@ -1,4 +1,16 @@
+# ===============================================
 
+# What should I have done?
+
+# Write a function called convert that takes a string (str) as input
+# receives and converts every :) to 🙂 and every :( to 🙁 and returns
+# the same input and the rest of the text remains unchanged.
+# Then, in the same file, implement a function named main that asks the
+# user for input, then calls the convert function and displays the output
+# by transferring the user's input to that function.
+# [!] Don't forget to call the main function at the bottom of your file.
+
+# ===============================================
 
 def convert(text):
     text = text.replace(":)", "🙂")
@@ -13,7 +25,8 @@ def main():
 
 
 main()
-# === OutPut ===
+
+# OutPut :
 
 # [*] Type Your Feeling : Im Happy :)
 # Im Happy 🙂
