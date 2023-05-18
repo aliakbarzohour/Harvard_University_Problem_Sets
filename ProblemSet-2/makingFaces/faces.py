3acbdac13060c10fedabab766bb5d1f8f087e0c5
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-05-18 23:59:53 +0330

    Develope

--- ProblemSet-2/makingFaces/faces.py
+++ ProblemSet-2/makingFaces/faces.py
@@ -14,7 +14,10 @@
 
 def convert(text):
     text = text.replace(":)", "🙂")
+    text = text.replace(":))", "😂")
     text = text.replace(":(", "🙁")
+    text = text.replace(":|", "😐")
+    text = text.replace("():)", "😇")
     return text
 
 
