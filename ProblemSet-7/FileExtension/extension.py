2742920adb7f40f56f110e0e7f5cb5d14088e043
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-05-24 20:17:44 +0330

    extensions code

---
+++ ProblemSet-7/FileExtension/extension.py
@@ -0,0 +1,21 @@
+
+filename = input("[$] Enter file name: ")
+
+if not "." in filename:
+    print("application/octet-stream")
+else:
+    ext = filename.split(".")[-1].strip()
+    if ext.lower() == "gif":
+        print("image/gif")
+    elif ext.lower() == "jpg" or ext.lower() == "jpeg":
+        print("image/jpeg")
+    elif ext.lower() == "png":
+        print("image/png")
+    elif ext.lower() == "txt":
+        print("text/plain")
+    elif ext.lower() == "pdf":
+        print("application/pdf")
+    elif ext.lower() == "zip":
+        print("application/zip")
+    else:
+        print("application/octet-stream")
