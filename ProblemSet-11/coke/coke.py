8e8a7c20ed438afaf9c7858549f155036c87a5a5
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-07-06 00:22:09 +0330

    finaly i found the best way

--- ProblemSet-11/coke/coke.py
+++ ProblemSet-11/coke/coke.py
@@ -1,9 +1,20 @@
-amount_due = 50
-while amount_due > 0:
-    print ("Amount due :", amount_due)
-    coin = int(input("Insert coin: "))
-    if coin in [25, 5, 10]:
-        amount_due -= coin
+coins = [25, 10, 5]
+price = 50
+paid = 0
 
-change_owned = abs (amount_due)
-print("Change owned: " , change_owned)
+while True:
+    print("Insert Coin")
+    coin = int(input())
+    if coin not in coins:
+        print("Amount Due: 50")
+    paid += coin
+    if paid < price:
+        print(f"Amount Due: {price - paid}")
+    else:
+        change = paid - price
+        print(f"Change Owed: {change}")
+        print("Thank you for your purchase!")
+        break
+
+if paid < price:
+    print(f"Amount Due: {price - paid}")
